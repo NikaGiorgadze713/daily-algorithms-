0220c5f1c94365e5cc5647d5ccb6a6c0c2ab100d
NikaGiorgadze713 <giorgadzenika963@gmail.com>
2026-05-21 00:14:42 +0400

    add best time to buy and sell stock solution

---
+++ arrays/Best Time to Buy and Sell Stock.py
@@ -0,0 +1,18 @@
+def maxProfit(prices):
+    buy, sell = 0, 1
+    maxP = 0
+
+    while sell < len(prices):
+
+        if prices[buy] < prices[sell]:
+            profit = prices[sell] - prices[buy]
+            maxP = max(maxP, profit)
+        else:
+            buy = sell
+
+        sell += 1
+
+    return maxP
+
+
+print(maxProfit([7,1,5,3,6,4]))
