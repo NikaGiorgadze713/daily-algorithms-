861cf0bb6cd3e2294b3bac7ee1c8f74a343e6983
NikaGiorgadze713 <giorgadzenika963@gmail.com>
2026-06-03 23:58:12 +0400

    add it to the algorithms

--- Strings/14. Longest Common Prefix.py
+++ Strings/14. Longest Common Prefix.py
@@ -8,3 +8,27 @@
 
 #Input: strs = ["flower","flow","flight"]
 #Output: "fl"
+
+def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+            
+        
+        empty = strs[0]
+        matching = []
+        
+        
+        for i in range(len(empty)):
+            
+            for k in range(1, len(strs)):
+                
+                
+                if i >= len(strs[k]) or empty[i] != strs[k][i]:
+                    
+                    return "".join(matching)
+            
+            
+            matching.append(empty[i])
+            
+        
+        return "".join(matching)
