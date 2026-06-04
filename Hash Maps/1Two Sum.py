2f131521ab5f5802a1963586a1771ec46c5a88ec
NikaGiorgadze713 <giorgadzenika963@gmail.com>
2026-06-04 23:31:27 +0400

    add it to the algorithms

---
+++ Hash Maps/1Two Sum.py
@@ -0,0 +1,29 @@
+# description 
+#Given an array of integers nums and an integer target, 
+# return indices of the two numbers such 
+# that they add up to target.
+
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+#You can return the answer in any order.
+
+ 
+
+#Example 1:
+
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+def twoSum(self, nums, target):
+        
+        empty = []
+        for i in range(len(nums)):
+            complement = target - nums[i]
+    
+            if complement in nums[i+1:]:
+        
+                x = nums.index(complement, i + 1)
+                empty.append(i)
+                empty.append(x)
+        return empty 
